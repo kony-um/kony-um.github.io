76d65fd04b0d71015c9c770a7a122ae12772f8bd
kony-um <yuma1102eff@gmail.com>
2023-08-19 05:56:34 +0900

    スマホ対応

--- smash/smash-plotter/script.js
+++ smash/smash-plotter/script.js
@@ -12,6 +12,50 @@ draggableItems.forEach(item => {
     item.addEventListener('dragend', e => {
         draggedItem = null;
     });
+
+    item.addEventListener('touchstart', e => {
+        draggedItem = e.target;
+        let touch = e.touches[0];
+        startX = touch.clientX - parseFloat(draggedItem.style.left || 0);
+        startY = touch.clientY - parseFloat(draggedItem.style.top || 0);        
+        e.preventDefault();
+    });
+    item.addEventListener('touchmove', e => {
+        if (!draggedItem) return;
+        let touch = e.touches[0];
+        draggedItem.style.left = `${touch.clientX - startX}px`;
+        draggedItem.style.top = `${touch.clientY - startY}px`;
+        e.preventDefault();
+    });
+    item.addEventListener('touchend', e => {
+        if (!draggedItem) return;
+        rect = mappingArea.getBoundingClientRect();
+        let left = parseFloat(draggedItem.style.left) + startX;
+        let top = parseFloat(draggedItem.style.top) + startY;
+        if (
+            // マッピングエリア内に入った場合
+            left >= rect.left &&
+            left < rect.right &&
+            top >= rect.top &&
+            top < rect.bottom
+        ) {
+            const touch = e.changedTouches[0];
+            const x = touch.clientX - rect.left;
+            const y = touch.clientY - rect.top;
+            draggedItem.style.left = `${x - (draggedItem.offsetWidth / 2)}px`;
+            draggedItem.style.top = `${y - (draggedItem.offsetHeight / 2)}px`;
+            draggedItem.crossOrigin = "anonymous"; 
+            mappingArea.appendChild(draggedItem);
+        } else {
+            // マッピングエリア外に出た場合
+            draggedItem.style.left = ''; // スタイルをリセット
+            draggedItem.style.top = '';  // スタイルをリセット
+            nonImageArea.appendChild(draggedItem);
+        }
+
+        draggedItem = null;
+        e.preventDefault();
+    });
 });
 
 nonImageArea.addEventListener('dragover', e => {
